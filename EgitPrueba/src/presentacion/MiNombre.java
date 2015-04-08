e2b7df111ef20cd969aa90ebd83d68e50ff9329f
hs1974g <hs1974g@unity7>
2015-04-08 15:39:27 +0200

    Commit inicial

---
+++ EgitPrueba/src/presentacion/MiNombre.java
@@ -0,0 +1,11 @@
+package presentacion;
+
+public class MiNombre {
+
+	public static void main(String[] args) {
+		// TODO Apéndice de método generado automáticamente
+
+		System.out.println("Mi nombre es Héctor Sales LLamas");
+	}
+
+}
